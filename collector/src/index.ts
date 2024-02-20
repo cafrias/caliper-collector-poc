b886833716e69489d208c176dc0ee5ae0723b0bb
Carlos Frias <carlos.a.frias@gmail.com>
2024-02-20 17:02:39 -0300

    feat: add envelope endpoint

--- collector/src/index.ts
+++ collector/src/index.ts
@@ -1,24 +1,53 @@
 import Fastify from "fastify";
+import rawBody from "fastify-raw-body";
+import { exit } from "node:process";
+import { saveToDataLake } from "./data-lake/data-lake";
+import { Envelope } from "./models/envelope";
+import { processEnvelope } from "./aggregator/aggregator";
+
 const fastify = Fastify({
   logger: true,
 });
 
-fastify.route({
-  method: "POST",
-  url: "/",
-  handler: async (request, reply) => {
-    // TODO: Validate the request includes a valid envelope
-    const envelope = request.body;
+fastify.register(rawBody);
 
-    // Write to filesystem
-    // Here we'll store the Caliper Envelopes as they are received
-    // in the local filesystem. This can be later migrated to
-    // other tools like Hadoop for ETL.
+fastify.after((err) => {
+  if (err) {
+    console.error(err);
+    exit(1);
+  }
 
-    // Aggregate data to database
+  fastify.route({
+    method: "POST",
+    url: "/",
+    handler: async (request, reply) => {
+      if (!request.rawBody) {
+        return reply.code(400).send();
+      }
+      // TODO: Validate the request includes a valid envelope
+      const envelope = request.body as Envelope;
 
-    return { hello: "world" };
-  },
+      const result = await Promise.allSettled([
+        saveToDataLake(envelope.sendTime, request.rawBody.toString()),
+        processEnvelope(envelope),
+      ]);
+
+      result.forEach((res) => {
+        if (res.status === "rejected") {
+          console.warn(res.reason);
+        }
+      });
+
+      // Write to filesystem
+      // Here we'll store the Caliper Envelopes as they are received
+      // in the local filesystem. This can be later migrated to
+      // other tools like Hadoop for ETL.
+
+      // Aggregate data to database
+
+      reply.code(201).send();
+    },
+  });
 });
 
 fastify
